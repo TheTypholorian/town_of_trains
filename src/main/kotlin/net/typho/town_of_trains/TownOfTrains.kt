15b7e15fd01c02eb6e20d7996edc8149cdb9d3d3
The Typhothanian <thetyphothanian@gmail.com>
2025-11-30 13:13:58 -0700

    Initial commit

---
+++ src/main/kotlin/net/typho/town_of_trains/TownOfTrains.kt
@@ -0,0 +1,9 @@
+package net.typho.town_of_trains
+
+import net.fabricmc.api.ModInitializer
+
+class TownOfTrains : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/net/typho/town_of_trains/client/TownOfTrainsClient.kt
@@ -0,0 +1,9 @@
+package net.typho.town_of_trains.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class TownOfTrainsClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
